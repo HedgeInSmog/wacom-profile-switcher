d6a27a01dee1e614179068b26796ba6e1fc2387c
ab <anton.burkun@gmail.com>
2016-03-28 21:07:21 +0300

    upd

--- wacom-permissions.sh
+++ wacom-permissions.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 sleep 1
-#rm /tmp/wacom_permissions_err.log
-/bin/chmod 666 /sys/bus/usb/devices/*/wacom_led/status_led0_select 2>>/tmp/wacom_permissions_err.log
+/bin/chmod 666 /sys/bus/hid/devices/*/wacom_led/status_led0_select 2>>/tmp/wacom_permissions_err.log
 
